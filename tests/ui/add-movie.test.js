23e8bdcc29c64805c157d9f8690552f222718bb2
PhilipKolarov <PhilipKolarov@students.softuni.bg>
2023-08-11 19:26:55 +0300

    fixing exact urls in ui tests

--- tests/ui/add-movie.test.js
+++ tests/ui/add-movie.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check add movie page', async ({ page }) => {
-    await page.goto('https://philipkolarov-movies.onrender.com/');
+    await page.goto('https://philipkolarov-movies.onrender.com/add-movie');
     const form = await page.$('form');
     expect(form).toBeTruthy();
   });
