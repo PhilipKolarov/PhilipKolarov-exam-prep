cf8e8f4d50eb2e4092aee802da47a3eab53637f3
PhilipKolarov <PhilipKolarov@students.softuni.bg>
2023-08-11 19:19:22 +0300

    fixing unit tests

--- tests/unit/home.tests.js
+++ tests/unit/home.tests.js
@@ -3,13 +3,13 @@ const fetch = require('node-fetch');
 
 suite('Home page', function() {
   test('Page title', async function() {
-    let res = await fetch("https://philipkolarov-movies.onrender.com/");
+    let res = await fetch("http://localhost:8888/");
     let body = await res.text();
     assert.ok(body.includes("<h1>Movies Collection</h1>"));
   });
   
   test('Movies count', async function() {
-    let res = await fetch("https://philipkolarov-movies.onrender.com/");
+    let res = await fetch("http://localhost:8888/");
     let body = await res.text();
     assert.ok(body.includes("Added movies: <b>3</b>"));
   });
